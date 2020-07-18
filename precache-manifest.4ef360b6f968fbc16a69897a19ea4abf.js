e9acfe7ba79a72d1f55f0df01b574a5c4ada1273
iMac User <marsara77@hotmail.co.uk>
2020-07-18 12:14:15 +0100

    Updates

---
+++ precache-manifest.4ef360b6f968fbc16a69897a19ea4abf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e8105bc68572670362437969d588e7d",
+    "url": "/Smarca02-module-ii/index.html"
+  },
+  {
+    "revision": "be20fa17d1dc4e393a50",
+    "url": "/Smarca02-module-ii/static/css/main.dc0cba22.chunk.css"
+  },
+  {
+    "revision": "d135b58dccaf3b54d94e",
+    "url": "/Smarca02-module-ii/static/js/2.4bb493a9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Smarca02-module-ii/static/js/2.4bb493a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be20fa17d1dc4e393a50",
+    "url": "/Smarca02-module-ii/static/js/main.6564845a.chunk.js"
+  },
+  {
+    "revision": "a158fa9bfffdac86da14",
+    "url": "/Smarca02-module-ii/static/js/runtime-main.455ba15d.js"
+  }
+]);
