6ee4fdda4d803498abae71503ba39fcc166a7e7d
iMac User <marsara77@hotmail.co.uk>
2020-07-22 14:12:32 +0100

    changed footer

--- src/components/Socials/index.jsx
+++ src/components/Socials/index.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
+import "./socials.css";
 
-const Socials = () => <div className="socials-container">socials</div>;
+const Socials = () => (
+  <div className="socials-container">
+    <button className="facebook social">
+      <FaFacebookF />
+    </button>
+
+    <button className="instagram social">
+      <FaInstagram />
+    </button>
+
+    <button className="twitter social">
+      <FaTwitter />
+    </button>
+  </div>
+);
 
 export default Socials;
